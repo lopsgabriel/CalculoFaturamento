c4d8c0ab5092521422feda4ca8dc90a8b67cdc89
Gabriel Lopes <souzsgabriel12@gmail.com>
2024-09-22 13:03:56 -0300

    .

--- calculo_faturamento.py
+++ calculo_faturamento.py
@@ -2,6 +2,7 @@ import json
 
 with open('faturamento.json', 'r') as file:
     dados = json.load(file)
+    #abre o arquivo .json
 
 faturamentos = [
     dia['valor'] for dia in dados['dias'] if dia['valor'] > 0
@@ -11,11 +12,13 @@ mais_faturamento = max(faturamentos)
 menos_faturamento = min(faturamentos)
 
 media_faturamento = sum(faturamentos) / len(faturamentos)
+#calcula a media de faturamento
 
 acima_da_media = 0
 for faturamento in faturamentos:
     if faturamento > media_faturamento:
         acima_da_media =+ 1
+#calcula os dias acima da media
 
 print(f"Dia com menor faturamento: {menos_faturamento}")
 print(f"Dia com maior faturamento: {mais_faturamento}")
