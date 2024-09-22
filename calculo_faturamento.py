b60061d8f0ff9928f05f075f7a0b3e1eef139315
Gabriel Lopes <souzsgabriel12@gmail.com>
2024-09-22 12:48:57 -0300

    Calculo de faturamento de uma empresa

---
+++ calculo_faturamento.py
@@ -0,0 +1,22 @@
+import json
+
+with open('faturamento.json', 'r') as file:
+    dados = json.load(file)
+
+faturamentos = [
+    dia['valor'] for dia in dados['dias'] if dia['valor'] > 0
+    ]  # Pula os dias sem faturamento
+
+mais_faturamento = max(faturamentos)
+menos_faturamento = min(faturamentos)
+
+media_faturamento = sum(faturamentos) / len(faturamentos)
+
+acima_da_media = 0
+for faturamento in faturamentos:
+    if faturamento > media_faturamento:
+        acima_da_media =+ 1
+
+print(f"Dia com menor faturamento: {menos_faturamento}")
+print(f"Dia com maior faturamento: {mais_faturamento}")
+print(f"Dias com faturamento acima da média: {acima_da_media}")
